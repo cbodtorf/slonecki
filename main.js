053175e0a6b39fc748e85c9988b1e89af03c3585
Cbodtorf <cbodtorf@gmail.com>
2016-11-27 19:36:54 -0500

    initial commit basic nav

---
+++ main.js
@@ -0,0 +1,20 @@
+(function(){
+  document.addEventListener('load', function(){
+    app.init()
+  })
+
+  var app = {
+    init: function(){
+      this.styles()
+      this.events()
+    },
+
+    styles: function(){
+
+    },
+
+    events: function(){
+      
+    }
+  }
+})()
